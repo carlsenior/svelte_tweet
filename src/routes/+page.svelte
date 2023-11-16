d6a6d764e393d2815098efa240e61479d6766b56
carleric <carl.senior.dev@hotmail.com>
2023-11-16 22:09:05 +0900

    Landing Page & Global Style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    // your script goes here
+    import {fly} from 'svelte/transition'
+</script>
+
+<svelte:head>
+    <title>Twittr</title>
+</svelte:head>
+
+<main class="container" in:fly={{ x:-100, duration:250, delay:300 }} out:fly={{ x:-100, duration:250 }}>
+    <section class="hero">
+        <h1 class="title">Twittr 🐦</h1>
+        <p class="text">Share your hot take with everyone.</p>
+    </section>
+    <section class="login">
+        <a href="/home" class="btn">💥 Share your Hot Take</a>
+    </section>
+</main>
+
+<style>
+    .container {
+        height: 100vh;
+        display: grid;
+    }
+    .hero,
+    .login {
+        display: grid;
+        place-content: center;
+    }
+
+    .hero {
+        background-color: var(--color-brand);
+        z-index: 2;
+    }
+    .title {
+        font-size: var(--font-80);
+        z-index: 2;
+    }
+    .text {
+        padding: var(--space-16);
+        transform: rotate(2deg) translateY(-40%);
+        background: var(--color-bg-primary);
+        font-weight: bold;
+        font-size: var(--font-24);
+        z-index: 1;
+    }
+    @media (min-width: 1024px) {
+        .container {
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
+</style>
